151b8e003ebbf8cf76d2d8a06486a7a7ca6baaff
Sanjay Kannan <whaatt@gmail.com>
2019-06-12 22:53:50 -0700

    Add ignoring of constants file.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ git config filter.nbstripout.clean 'nbstripout'
 git config filter.nbstripout.smudge cat
 git config filter.nbstripout.required true
 git config diff.ipynb.textconv 'nbstripout -t'
+git update-index --assume-unchanged constants.py
